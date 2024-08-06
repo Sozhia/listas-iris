cff28e3bf2cca24331d3f7280c452e6811deab92
Miqueas Garcia <mqggonzalez@gmail.com>
2024-08-06 11:18:56 +0100

    One of many posibilities with data structures

--- main/sample/project.py
+++ main/sample/project.py
@@ -3,20 +3,36 @@ import math
 import pathlib
 import tkinter as tk   # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
+from tkinter import filedialog
 
-def load_file(url):
-  with open (url, 'r') as file:
+def load_dataset(datafile):
+  with open (datafile, 'r') as file:
     csv_reader = csv.reader(file)
     dataset = [row for row in csv_reader if row]
   return dataset
 
-
-from tkinter import filedialog
-
+def format_dataset(dataset):
+  
+  formated_dataset= []
+  
+  for row in dataset:
+    flower_type = row[-1]
+    
+    if flower_type  not in formated_dataset:
+      formated_dataset.append(flower_type)
+    
+    flower_data = list(row[:-1])
+    flower_index = formated_dataset.index(flower_type) + 1
+    
+    formated_dataset.insert(flower_index, flower_data)
+  return formated_dataset
+     
 root = tk.Tk()
 root.withdraw()
 
 file_path = filedialog.askopenfilename()
 
-dataset = load_file(file_path)
-print(dataset)
+rawdataset = load_dataset(file_path)
+
+print(format_dataset(rawdataset))
+
